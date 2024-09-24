dc460827f599041602b18d9ecc4bd6379abc4af8
Aliaksandr Pletsiazhou <alexandr.pletezhov@gmail.com>
2024-09-24 20:54:39 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const swaggerConfig = new DocumentBuilder()
+  .setTitle('Task Manager API')
+  .setDescription('API documentation for Task Manager project')
+  .setVersion('0.0.1')
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(port);
+
+  return port;
 }
 
-bootstrap().catch((error) => console.error(error));
+bootstrap()
+  .then((port) => console.log(`Server has been started on ${port} port.`))
+  .catch((error) => console.error(error));
